24ea241c07259c240d20b5a24b69521271b1b343
andrewjordan <drewjordan@live.com>
2015-10-12 22:18:40 -0400

    started on blog work

---
+++ EntityFrameworkDemo/Models/Invoice.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EntityFrameworkDemo.Models
+{
+    public class Invoice
+    {
+        public int ID { get; set; }
+        public decimal AmountDue { get; set; }
+        public DateTime DueDate { get; set; }
+        public int ClientID { get; set; }
+        public int ProjectID { get; set; }
+
+        public virtual Client Client { get; set; }
+        public virtual Project Project { get; set; }
+    }
+}
